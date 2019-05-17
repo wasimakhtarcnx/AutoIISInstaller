56d1b63d1618f4d268a506e885d7bb73c63ad3b2
Wasim Akhtar <Wasim.Akhtar@concentrix.com>
2019-05-18 01:04:14 +0530

    Updated

--- AutoIISInstaller/Program.cs
+++ AutoIISInstaller/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Web.Administration;
 using System;
+using System.Reflection;
 
 namespace AutoIISInstaller
 {
@@ -10,9 +11,25 @@ namespace AutoIISInstaller
             string smsPool = "sms.local";
             ApplicationPool applicationPool;
             System.Console.WriteLine("IIS Installer Started");
+            string appLocation = "";
+            try
+            {
+                Console.WriteLine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+                appLocation = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            }
+            catch (System.IO.IOException)
+            {
+            }
+            catch (Exception)
+            {
+            }
+
             CreateApplicationPool createApplicationPool = new CreateApplicationPool();
             applicationPool = createApplicationPool.CreatePoolIIS(smsPool);
             CreateWebsite createWebsite = new CreateWebsite();
+            createWebsite.AddWebsite("ncstcsmsoffline", @"C:\inetpub\ncstcsmsoffline\", applicationPool);
+            RunCMDCommand runCMDCommand = new RunCMDCommand();
+            runCMDCommand.CopyFiles(@"C:\Users\wasim.akhtar\Documents\TemWebsite\", @"C:\inetpub\ncstcsmsoffline\");
 
             System.Console.WriteLine("IIS Installer Ended");
             Console.ReadLine();
---
+++ AutoIISInstaller/RunCMDCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace AutoIISInstaller
+{
+    public class RunCMDCommand
+    {
+        public void ExecuteCommand(string ToPath)
+        {
+            System.Diagnostics.Process process1 = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo1 = new System.Diagnostics.ProcessStartInfo();
+            startInfo1.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo1.FileName = "cmd.exe";
+            startInfo1.Arguments = "/C mkdir " + ToPath;
+            process1.StartInfo = startInfo1;
+            process1.Start();
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C xcopy " + @"C:\Users\wasim.akhtar\Documents\TemWebsite\ " + ToPath + " /h/i/c/k/e/r/y";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+        public void CopyFiles(string SourcePath, string DestinationPath)
+        {
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+        }
+    }
+}
