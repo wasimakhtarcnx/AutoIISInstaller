5f3e4b6d61f11f78531f9e2fcd037f8be79848a3
Wasim Akhtar <Wasim.Akhtar@concentrix.com>
2019-05-17 23:49:25 +0530

    IIs Server

--- AutoIISInstaller/Program.cs
+++ AutoIISInstaller/Program.cs
@@ -5,6 +5,7 @@ namespace AutoIISInstaller
         static void Main(string[] args)
         {
             System.Console.WriteLine("IIS Installer Started");
+            System.Console.WriteLine("IIS Installer Ended");
         }
     }
 }
