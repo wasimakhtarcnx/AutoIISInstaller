d2cc8e45a8bf4a619cc31808028d333f21f8c480
Wasim Akhtar <Wasim.Akhtar@concentrix.com>
2019-05-25 23:10:46 +0530

    Added Exception

--- AutoIISInstaller/Program.cs
+++ AutoIISInstaller/Program.cs
@@ -8,34 +8,38 @@ namespace AutoIISInstaller
     {
         static void Main(string[] args)
         {
-            string smsPool = "schoolmanagement";
-            ApplicationPool applicationPool;
-            System.Console.WriteLine("IIS Installer Started");
-            string appLocation = "";
             try
             {
+                string smsPool = "schoolmanagement";
+                ApplicationPool applicationPool;
+                System.Console.WriteLine("IIS Installer Started");
+                string appLocation = "";
+
                 Console.WriteLine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
                 appLocation = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
                 appLocation = appLocation + @"\Publish\";
+
+
+                CreateApplicationPool createApplicationPool = new CreateApplicationPool();
+                applicationPool = createApplicationPool.CreatePoolIIS(smsPool);
+                CreateWebsite createWebsite = new CreateWebsite();
+                createWebsite.AddWebsite("schoolmanagement", @"C:\inetpub\schoolmanagement\", applicationPool);
+                RunCMDCommand runCMDCommand = new RunCMDCommand();
+                runCMDCommand.CopyFiles(appLocation, @"C:\inetpub\schoolmanagement\");
+
+                System.Console.WriteLine("IIS Installer Ended");
+
+
+                System.Diagnostics.Process.Start("http://localhost/");
             }
-            catch (System.IO.IOException)
+            catch (System.IO.IOException ex)
             {
+                Console.WriteLine(ex.Message);
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                Console.WriteLine(e.Message);
             }
-
-            CreateApplicationPool createApplicationPool = new CreateApplicationPool();
-            applicationPool = createApplicationPool.CreatePoolIIS(smsPool);
-            CreateWebsite createWebsite = new CreateWebsite();
-            createWebsite.AddWebsite("schoolmanagement", @"C:\inetpub\schoolmanagement\", applicationPool);
-            RunCMDCommand runCMDCommand = new RunCMDCommand();
-            runCMDCommand.CopyFiles(appLocation, @"C:\inetpub\schoolmanagement\");
-
-            System.Console.WriteLine("IIS Installer Ended");
-
-
-            System.Diagnostics.Process.Start("http://localhost/");
             Console.ReadLine();
 
         }
